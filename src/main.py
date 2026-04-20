7ba52279e46dbb2a4f6741c91a8fa0a0a11eb326
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2026-04-20 20:57:42 +0300

    create text node

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    # Example usage
+    node1 = TextNode("Hello, World!", TextType.TEXT)
+    node2 = TextNode("Click here", TextType.URL, url="https://example.com")
+    print(node1)
+    print(node2)
+
+
+if __name__ == "__main__":
+    main()
